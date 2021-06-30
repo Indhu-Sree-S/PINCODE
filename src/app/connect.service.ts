f20a176f3cdef38dfbe2921a6a3fe92a382d91db
Indhu Sree S <indhusrees16@gmail.com>
2021-06-30 20:54:49 +0530

    commit first

--- src/app/connect.service.ts
+++ src/app/connect.service.ts
@@ -7,14 +7,21 @@ import { content } from './connect';
   providedIn: 'root'
 })
 export class ConnectService {
-  private url = '';
+  private url: number;
+  private full: string;
 
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any>{
     // eslint-disable-next-line no-underscore-dangle
-    return this.http.get('https://api.postalpincode.in/pincode/635109');
+    this.full = 'https://api.postalpincode.in/pincode/'+ this.url;
+    return this.http.get(this.full);
+  }
+
+  geturl(value: number) {
+    this.url = value;
+    alert('I am Here..');
   }
 
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,25 +13,12 @@ export class HomePage {
   lst: content[];
   lst1: one;
   url: any;
+  ab = 0;
 
   constructor(private apiservice: ConnectService) {}
 
   // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
-  ngOnInit() {
-    this.apiservice.getData()
-    .subscribe
-    (
-      data=>
-      {
-        this.lst = data;
-        //this.lst2 = this.lst1;
-        console.log(data);
-      }
-
-    );
-
-
-  }
+  ngOnInit() {}
 
   Name(y: any) {
     this.lst1 = y;
@@ -50,7 +37,7 @@ export class HomePage {
 
   Division(y: any) {
     this.lst1 = y;
-    return this.lst1.Division;
+    return this.lst1.State;
   }
 
   checkfirst(event)
@@ -64,6 +51,19 @@ export class HomePage {
   sendtoser(){
     console.log(this.url);
     alert(this.url +' is your PINCODE');
+    this.apiservice.geturl(this.url);
+
+    this.apiservice.getData()
+    .subscribe
+    (
+      data=>
+      {
+        this.lst = data;
+
+        console.log(data);
+      }
+
+    );
   }
 
 }
