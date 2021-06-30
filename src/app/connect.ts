277d90575e3db79259ba2ee9c2a4190a77ce6e3d
Indhu Sree S <indhusrees16@gmail.com>
2021-06-30 17:35:12 +0530

    commit first

---
+++ src/app/connect.ts
@@ -0,0 +1,21 @@
+//import { content1 } from './PostOffice';
+/* eslint-disable @typescript-eslint/naming-convention */
+export class content {
+  Message: string;
+  Status: string;
+  PostOffice: one[];
+
+}
+
+export class one {
+  Name: string;
+  Description: string;
+  BranchType: string;
+  DeliveryStatus: string;
+  Circle: string;
+  District: string;
+  Division: string;
+  Region: string;
+  State: string;
+  Country: string;
+}
